f11850d58073b3c30043691f3b84e098c86b3916
davidapud <dapud@hotmail.com.ar>
2022-01-20 20:04:14 -0300

    entrega 20 01 22

--- javascript/codigo.js
+++ javascript/codigo.js
@@ -15,7 +15,7 @@
                   alert(" El viaje es de tipo " + this.tipo + ", el mejor destino para tu eleccion es " + this.destino + ", la duracion del viaje es de " + this.dias + " dias, incluye  " + this.pension + " y el valor por persona es de $" + this.precio + ".-");
                   } 
               }
-              
+            
               
           
                const oferta1 = new oferta ("aventura", "mendoza", 10, 20000, "media pension ");
@@ -24,7 +24,32 @@
                const oferta4 = new oferta ("diversion", "san carlos de bariloche", 25, 60000, "pension completa");
                const oferta5 = new oferta ("cultura", "neuquen", 5, 5000, "media pension");
                
+               const oferta6 = new oferta ("Relax","san martin de los andes",30,80000," pension completa");
+               const oferta7 = new oferta ("Relax","Chaco",15,45000," pension completa");
+               const oferta8 = new oferta ("Diversion","Chubut",15,45000,"Media pension");
+               const oferta9 = new oferta ("Romantico","Córdoba"   ,15,45000," pension completa");
+               const oferta10 = new oferta ("Cultural","Corrientes"   ,15,45000," desayuno");
+               const oferta11 = new oferta ("Aventura","Entre Ríos",7,15000,"pension completa");
+               const oferta12 = new oferta ("Relax","Formosa",7,15000,"desayuno");
+               const oferta13 = new oferta ("Diversion","Jujuy",7,15000,"pension completa");
+               const oferta14 = new oferta ("Romantico","La Pampa",7,15000,"Media pension");
+               const oferta15 = new oferta ("Cultural","La Rioja",7,15000,"desayuno");
+               const oferta16 = new oferta ("Aventura","Mendoza",25,70000,"Media pension");
+               const oferta17 = new oferta ("Relax","Misiones",25,70000,"pension completa");
+               const oferta18 = new oferta ("Diversion","Neuquén",25,70000,"Media pension");
+               const oferta19 = new oferta ("Romantico","Río Negro",25,70000,"completa");
+               const oferta20 = new oferta ("Cultural","Salta",25,70000,"Media pension");
+               const oferta21 = new oferta ("Aventura","San Juan",10,35000,"pension completa");
+               const oferta22 = new oferta ("Relax","San Luis",10,35000,"Media pension ");
+               const oferta23 = new oferta ("Diversion","Santa Cruz",10,35000,"media pension");
+               const oferta24 = new oferta ("Romantico","Santa Fe",10,35000,"desayuno");
+               const oferta25 = new oferta ("Cultural","Santiago del Estero",10,35000,"pension completa");
+               const oferta26 = new oferta ("Aventura","Tierra del Fuego",10,35000,"Media pension");
+               const oferta27 = new oferta ("Relax","Tucumán",10,35000,"pension completa");
                
+              
+
+
                
                
                   pedido = prompt  ("que tipo de viaje quieres tener? elige: 1-Aventura, 2-Relax, 3-Romantico, 4-Diversion o 5-Cultural " ) ;
@@ -81,4 +106,20 @@
           
           alert(destinoNacional.join("\n"));
           alert ("te lo ordeno alfabeticamente para que sea mas facil encontrar tu destino")
-          alert(destinoNacional.sort());
+          alert(destinoNacional.sort());  
+          alert ("que tipo de viaje quieres emprender");
+
+
+       
+         
+
+
+         const nuevosDestinos = document.getElementById ("nuevosDestinos");
+         for( let i= 0; i< destinoNacional.length; i++) {
+         const li  = document.createElement ('li');
+         li.textContent = destinoNacional[i];
+         nuevosDestinos.appendChild(li);
+         console.log (nuevosDestinos)
+         
+        }
+        
